2698a38d52be3ef47055c4e54d215d8bd422e1d4
Anoreloth <93758322+Anoreloth@users.noreply.github.com>
2021-12-19 23:24:28 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        string path = @"d:\temp\";
+
+        if (!File.Exists(path + "g.txt"))
+        {
+            using (StreamWriter gf = File.CreateText(path + "g.txt"))
+            {
+                using (StreamReader ff = File.OpenText(path + "f.txt"))
+                {
+                    string s;
+                    while ((s = ff.ReadLine()) != null)
+                    {
+                         gf.WriteLine(s.ToLower());
+                    }
+                }
+            }
+        }
+    }
+}
